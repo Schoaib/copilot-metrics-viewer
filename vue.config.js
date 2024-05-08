8a02d0f0893d4e5ed1cae5ac74c74287db19fc4b
Muhammad Shoaib Syed <hello@syedshoaib.com>
2024-05-08 22:54:29 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/server/'
+    ? '/copilot-metrics-viewer/'
     : '/',
   pluginOptions: {
     vuetify: {

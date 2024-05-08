eeb186b89c25465c0c4726c41c725062b14d504b
Syed Shoaib <hello@syedshoaib.com>
2024-05-08 19:58:43 +0400

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/server/'
+    : '/',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
